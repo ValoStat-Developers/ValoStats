7d49cd43e1ba0b1fc1fa801d610d43064c323225
khudaykulovamir <khudaykulovamir@gmail.com>
2021-04-22 00:35:53 +0500

    ValoApi1

--- ValoStats/ApiCaller.swift
+++ ValoStats/ApiCaller.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+let valorantApiLink = URL(string: "https://developer.riotgames.com/apis#val-ranked-v1/GET_getLeaderboard")
+if let unwrappedURL = valorantApiLink {
+    var request = URLRequest(url: unwrappedURL)
+    request.addValue("71201fa9-7095-4c99-b675-28aef67d3f77", forHTTPHeaderField: "71201fa9-7095-4c99-b675-28aef67d3f77")
+    // Make your request and handle the response
+}
