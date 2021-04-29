24a8ab224405e29b393da50cc42389db4fe53a5c
Victor Dai <victor.dai.1@stonybrook.edu>
2021-04-29 16:06:46 -0400

    APICaller started

--- ValoStats/LoginViewController.swift
+++ ValoStats/LoginViewController.swift
@@ -13,6 +13,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var Tagline: UITextField!
     @IBOutlet weak var LoginButton: UIButton!
     
+    var gameName: String
+    var tagLine: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -25,7 +28,21 @@ class LoginViewController: UIViewController {
         let tagline = Tagline.text!
         let apiKey = "RGAPI-64bb0f96-e4af-4b3d-b5f4-7d78a26d969d"
         let url = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + riotId + "/" + tagline + "?api_key=" + apiKey
-        print(url)
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+        gameName = dataDictionary["gameName: "] as! String
+        tagLine = dataDictionary["tagLine: "] as! String
+        
+        
+        
     }
    
     /*
